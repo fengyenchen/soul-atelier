aa978ba73c0be592fc818621f281e6bedbf8cade
fengyenchen <feng20050504@gmail.com>
2026-06-27 08:40:46 +0800

    feat: Add background music

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import BgMusic from "@/components/bgMusic";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +43,10 @@ export default function RootLayout({
       lang="zh-TW"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <BgMusic />
+      </body>
     </html>
   );
 }
